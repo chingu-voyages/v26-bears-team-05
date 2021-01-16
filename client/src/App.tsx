9cb4bb8f479697bbf2a2da368eba3c36623aea88
Xarri Jorge <kodinglife@gmail.com>
2021-01-16 01:57:07 +0000

    Added Material-UI & project folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,12 @@ import './App.scss'
 
 function App(): JSX.Element {
     return (
-        <div className='App'>
-            <header className='App-header'>
+        <div className="App">
+            <header className="App-header">
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
             </header>
